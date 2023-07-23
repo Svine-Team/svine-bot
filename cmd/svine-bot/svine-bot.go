78245d754e581b17861761bd93cc8db58e234573
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-07-23 20:33:09 +0300

    feat(token): added token read from .env

--- cmd/svine-bot/svine-bot.go
+++ cmd/svine-bot/svine-bot.go
@@ -2,10 +2,42 @@ package main
 
 import (
 	"fmt"
+	"os"
+    "log"
 
 	"github.com/Svine-Team/svine-bot/pkg/app"
+    "github.com/joho/godotenv"
 )
 
+// Variables used for command line parameters
+var (
+	Token string
+)
+
+func init() {
+
+	// flag.StringVar(&Token, "t", "", "Bot Token")
+	// flag.Parse()
+    // godotenv.Load()
+}
+
+// Use godot package to load/read the .env file and
+//   return the value of the key.
+func getEnvVariable(key string) string {
+
+  // load .env file
+  err := godotenv.Load()
+
+  if err != nil {
+    log.Fatalf("Error loading .env file")
+  }
+
+  return os.Getenv(key)
+}
+
 func main() {
-	fmt.Println(app.Init())
+  // godotenv package
+  envVariable := getEnvVariable("BOT_TOKEN")
+
+  fmt.Printf("godotenv : %s = %s \n", "BOT_TOKEN", envVariable)
 }
