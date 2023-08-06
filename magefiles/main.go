65b93cf7de37a9f218dbaac55cc49e7eaddde8f5
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-08-06 13:52:43 +0300

    build: added air and mage for easier developing

---
+++ magefiles/main.go
@@ -0,0 +1,15 @@
+//go:build mage
+
+package main
+
+import (
+    "github.com/magefile/mage/sh"
+)
+
+func Build() error {
+    return sh.Run("go", "build", "-o", "tmp", "./...")
+}
+
+func BuildLive() error {
+    return sh.RunV("air")
+}
